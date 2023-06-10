88d8d0fc36fdfbd1e934ff40a6aff3698b489550
tadahiroueta <tadahiroueta@gmail.com>
2023-06-09 22:16:58 -0300

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ "./src/**/*.js" ],
-  theme: { extend: { colors: {}}},
-}
+  theme: { extend: { colors: {
+    background: "#121212",
+    search: "#34323D",
+    "search-background": "#232228",
+    "active-background": "#353340",
+    navigation: "#BEBEBE" 
+    // haha, bebe
+}}}}

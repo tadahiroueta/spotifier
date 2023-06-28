bffd6222104bc12c62857cd641f648455e1ddec9
tadahiroueta <tadahiroueta@gmail.com>
2023-06-27 19:44:59 -0500

    added styles for desktop search

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,20 +10,19 @@ export default function Layout({ children }) {
 
 			{ children }
 
-			<div className="fixed bottom-0 my-20 text-navigation text-xs font-light flex space-x-8">
+			<div className="fixed bottom-0 my-20 text-navigation text-xs font-light flex space-x-8 md:text-base">
 
-				<div onClick={ () => navigate("search") } className={ "w-24 flex flex-col items-center space-y-1 " + (pathname === "/search" ? "text-white" : null) }>
-					<MagnifyingGlassIcon className="h-8 w-8" />
+				<div onClick={ () => navigate("search") } className={ "w-24 flex flex-col items-center space-y-1 md:w-48 " + (pathname === "/search" ? "!text-white" : null) }>
+					<MagnifyingGlassIcon className="h-8 md:h-16" />
 					<div>Search</div>
 				</div>
 
-				<div onClick={ () => navigate("library") } className={ "w-24 flex flex-col items-center space-y-1 " + (pathname === "/search" ? null : "text-white") }>
-					<Square3Stack3DIcon className="h-8 w-8" />
+				<div onClick={ () => navigate("library") } className={ "w-24 flex flex-col items-center space-y-1 md:w-48 " + (pathname === "/search" ? null : "!text-white") }>
+					<Square3Stack3DIcon className="h-8 md:h-16" />
 					<div>Dev's Library</div>
 				</div>
 
 			</div>	
  
     </div>
-	)
-}
+)}
--- src/components/Search.js
+++ src/components/Search.js
@@ -44,51 +44,59 @@ export default function Search() {
   }
 
   return (
-    <div className={ 'min-h-screen w-full bg-background flex flex-col items-center ' + (searchTerm ? "!bg-active-background" : null) }>
+    <div className={ 'min-h-screen w-full bg-background ' + (searchTerm ? "!bg-active-background" : null) }>
 
-      <div className={ "pt-16 pb-4 w-full flex flex-col items-center " + (searchTerm ? "bg-search-background" : null) }>
-        <div className='w-5/6 space-y-3'>
+      <div className="mt-48 w-full flex flex-col md:flex-row-reverse items-center md:items-start md:justify-center">
 
-          { searchTerm ? null : <div className={ "text-white text-[1.7rem] font-semibold " }>Search</div> }
+        <div className='w-full md:w-1/3 flex flex-col items-center'>
 
-          <div className="w-full font-extralight flex items-center justify-between space-x-6">
+          <div className={ "pt-16 md:pt-0 pb-4 w-full flex flex-col items-center " + (searchTerm ? "bg-search-background md:bg-transparent" : null) }>
+            <div className='w-5/6 space-y-3'>
 
-            <div className={ "py-2 px-5 rounded-md flex-grow bg-white text-black flex items-center space-x-2 " + (searchTerm ? "px-1 h-7 bg-search text-white" : null)}>
+              { searchTerm ? null : <div className="text-white text-[1.7rem] font-semibold ">Search</div> }
 
-              <MagnifyingGlassIcon className={ "h-8 w-8 " + (searchTerm ? "h-6 w-6" : null) } />
+              <div className="w-full font-extralight flex items-center justify-between space-x-6">
 
-              <input type="text" placeholder='Find your public playlist' value={ searchTerm } onChange={ e => setSearchTerm(e.target.value) } onKeyUp={({ key }) => { if (key === "Enter") search(searchTerm) }} className="w-5/6 bg-transparent placeholder:text-black outline-none" />
+                <div className={ "py-2 px-5 rounded-md flex-grow bg-white text-black flex items-center space-x-2 " + (searchTerm ? "!px-1 h-7 !bg-search md:!bg-search-background !text-white" : null)}>
 
-              { searchTerm ? <XMarkIcon onClick={ () => setSearchTerm("") } className="h-6 w-6" /> : null }
+                  <MagnifyingGlassIcon className={ "h-8 " + (searchTerm ? "!h-6 " : null) } />
+
+                  <input type="text" placeholder='Find your public playlist' value={ searchTerm } onChange={ e => setSearchTerm(e.target.value) } onKeyUp={({ key }) => { if (key === "Enter") search(searchTerm) }} className="w-5/6 bg-transparent placeholder:text-black outline-none" />
+
+                  { searchTerm ? <XMarkIcon onClick={ () => setSearchTerm("") } className="h-6" /> : null }
+
+                </div>
+
+                { searchTerm ? <div onClick={ () => setSearchTerm("") } className="text-white">Cancel</div> : null }
+
+              </div>
 
             </div>
+          </div>
 
-            { searchTerm ? <div onClick={ () => setSearchTerm("") } className={ "text-white " }>Cancel</div> : null }
+          <div className="mt-6 w-5/6 flex flex-col space-y-4">
+            { !searchResults ? null : searchResults.map((result, i) => (
+              <div onClick={ () => handleClick(result) } key={ i } className="flex items-center space-x-4">
+                
+                <img src={ result.cover } alt={ result.name } className='w-14' />
 
+                <div className="">
+                  <div className="text-white">{ result.name }</div>
+                  <div className="text-navigation text-sm">{ result.artist }</div>
+                </div>
+
+              </div>
+            ))}
           </div>
 
         </div>
+
+        <a href={ playlist.link } className='mt-10 md:mt-0 w-7/12 md:w-1/6 space-y-5' >
+          <img src={ playlist.cover } alt="playlist-placeholder" className='w-full' />
+          <div className='text-center text-xl font-light text-white'>{ playlist.name }</div>
+        </a>
+
       </div>
-
-      <div className="mt-6 w-5/6 flex flex-col space-y-4">
-        { !searchResults ? null : searchResults.map((result, i) => (
-          <div onClick={ () => handleClick(result) } key={ i } className="flex items-center space-x-4">
-            
-            <img src={ result.cover } alt={ result.name } className='w-14' />
-
-            <div className="">
-              <div className="text-white">{ result.name }</div>
-              <div className="text-navigation text-sm">{ result.artist }</div>
-            </div>
-
-          </div>
-        ))}
-      </div>
-
-      <a href={ playlist.link } className='mt-10 w-7/12 space-y-5' >
-        <img src={ playlist.cover } alt="playlist-placeholder" />
-        <div className='text-center text-xl font-light text-white'>{ playlist.name }</div>
-      </a>
 
     </div>
 )}
